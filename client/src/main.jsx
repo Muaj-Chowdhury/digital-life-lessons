5a67625b5aac633b0d1b9a43e146ff173382063e
Your Name <you@example.com>
2026-07-20 23:51:00 +0600

    workflow testing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from './context/AuthProvider.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastContainer, toast } from 'react-toastify';
 
-const queryClient = new QueryClient()  //using react query for data fetching and caching
+const queryClient = new QueryClient()  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
